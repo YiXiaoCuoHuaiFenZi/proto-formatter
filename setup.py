3fdb5da9bfb18a5bd6a87ca8a83ba0c2727e0d16
一小撮坏分子 <33883485+YiXiaoCuoHuaiFenZi@users.noreply.github.com>
2021-08-19 11:01:55 +0800

    update version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ dev_requires = [
 
 setuptools.setup(
     name="proto-formatter",
-    version="0.0.2",
+    version="0.0.3",
     author="YiXiaoCuoHuaiFenZi(一小撮坏分子)",
     author_email="249664317@qq.com",
     description="Protocol Buffers file formatter.",
