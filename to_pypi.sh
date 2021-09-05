41a49681445317e338f3ca74d9fbc58974231bec
一小撮坏分子 <33883485+YiXiaoCuoHuaiFenZi@users.noreply.github.com>
2021-09-06 01:33:19 +0800

    fix right comment to top comment issue when specified max length

--- to_pypi.sh
+++ to_pypi.sh
@@ -2,7 +2,8 @@
 
 source .venv/bin/activate
 # build dist files
-python3 -m build
+#python3 -m build
+python3 setup.py sdist
 
 # use default twine config, upload package to pypi https://upload.pypi.org/legacy/
 python3 -m twine upload --repository pypi dist/*
